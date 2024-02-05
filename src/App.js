addb6d18563224f34d4267c96c3e3a81541f9da3
princesangani123 <princesangani598@gmail.com>
2024-02-05 08:38:31 +0530

    prince

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import StudentResult from './StudentResult';
+
+function App() {
+  return (
+    <div>
+        <StudentResult></StudentResult>
+    </div>
+  );
+}
+
+export default App;
